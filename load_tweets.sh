328a4c2ba60876f46d0a1d685081842f88b1942b
RyanL-N <ra.lee.nguyen@gmail.com>
2025-04-08 19:56:42 -0700

    Final working version: normalized + denormalized setup complete

--- load_tweets.sh
+++ load_tweets.sh
@@ -9,10 +9,12 @@ test-data.zip
 
 echo 'load normalized'
 for file in $files; do
+    python3 -u load_tweets.py --db "postgresql://postgres:pass@localhost:5434" --inputs $file
     # call the load_tweets.py file to load data into pg_normalized
 done
 
 echo 'load denormalized'
 for file in $files; do
+    unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:5433 -c "COPY tweets_jsonb (data) FROM STDIN csv quote E'\x01' delimiter E'\x02';"
     # use SQL's COPY command to load data into pg_denormalized
 done
